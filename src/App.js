99f331563175bb3c7d3542bea341b8597990aa58
mohammed ilyas <MY_NAME@example.com>
2021-12-01 16:46:17 +0530

    hackhon commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="ru">
+    <header>
+      <a className="logo"
+      href="#">Stackoverflowclone</a>
+      <form action="" className="search">
+        <input type="text" placeholder="search..."/>
+      </form>
+      <a className="profile" href="#">David</a>
+    </header>
+  </div>
   );
 }
 
